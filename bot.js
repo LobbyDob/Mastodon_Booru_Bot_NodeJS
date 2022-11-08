23eca1d12271614ae8c4297326495018958d0467
LobbyDob <117674960+LobbyDob@users.noreply.github.com>
2022-11-08 22:13:27 +0000

    Added NSFW content check in bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,13 @@ async function Post()
     const url = booru.url(post.file_url);
     const source = booru.url(post.source);
     const name = `${post.md5}.${post.file_ext}`;
+    // If the post's rating is questionable or explicit, flag it as NSFW
+    var isQuestionable = false;
+    if (post.rating == 'q' || post.rating == 'e')
+    {
+        isQuestionable = true;
+    }
+    
     const writeStream = fs.createWriteStream(name);
 
     //Download post's image
@@ -44,18 +51,24 @@ async function Post()
     //Allow time for write operation to finish
     setTimeout(function() {writeStream.close();}, 2000);
     //Upload image as a Mastodon post
-    setTimeout(function() {MastodonPost(name, source);}, 4000);
+    setTimeout(function() {MastodonPost(name, source, isQuestionable);}, 4000);
     //Delete image after uploading
     setTimeout(function() {DeleteFile(name);}, 6000);
 }
 
-function MastodonPost(name, source)
+function MastodonPost(name, source, isQuestionable)
 {
+    var statusString = `(Source: ${source})`;
+    if (isQuestionable)
+    {
+        statusString = `NSFW\n${statusString}`;
+    }
+    
     const readStream = fs.createReadStream(name);
     authenticatedMastodon.post('media', { file: readStream}).then(resp =>
         {
         const id = resp.data.id;
-        authenticatedMastodon.post('statuses', {status: `Source: ${source}`, media_ids: [id]}, (error, data) => 
+        authenticatedMastodon.post('statuses', {status: `${statusString}`, media_ids: [id]}, (error, data) => 
         {
             if (error)
             {
